e8f46fb53cb270a6f10ef96ac55fa405511b75be
Timothy <timothy.miles-board@london.ac.uk>
2015-06-26 09:23:03 +0100

    Add params for IIIF server, base path and doc field holding image location on server; attempt to deal with whole-part relations (ie. collections)

--- cfg/cfg.d/iiif-manifest.pl
+++ cfg/cfg.d/iiif-manifest.pl
@@ -1,1 +1,8 @@
+# the URL of the IIIF-compliant image server
+# $c->{plugins}{"Export::IIIFManifest"}{params}{iiif_server_url} = "";
+# the base path of images on the server
+# $c->{plugins}{"Export::IIIFManifest"}{params}{iiif_base_path} = "";
+# the field in the document object that contains the path and filename of the image on the server
+# $c->{plugins}{"Export::IIIFManifest"}{params}{iiif_path_field} = "";
+
 $c->{plugins}{"Export::IIIFManifest"}{params}{disable} = 0;
--- lib/plugins/EPrints/Plugin/Export/IIIFManifest.pm
+++ lib/plugins/EPrints/Plugin/Export/IIIFManifest.pm
@@ -24,20 +24,21 @@ sub new
 
 sub output_dataobj
 {
-	my( $plugin, $dataobj ) = @_;
+	my( $plugin, $eprint ) = @_;
 print STDERR "output dataobj\n";
 
 	my $repo = $plugin->repository;
-	my $id = $dataobj->uri;
+	my $id = $eprint->uri;
 
-	my $IMAGE_SERVER_URL = $plugin->param( 'image_server_url' ) || sprintf( '%s/%s', $repo->config( 'base_url' ), 'iiif');
-	my $IMAGE_SERVER_BASE = $plugin->param( 'image_server_base' ) || $dataobj->value( 'dir' );
+	my $IIIF_SERVER_URL = $plugin->param( 'iiif_server_url' ) || sprintf( '%s/%s', $repo->config( 'base_url' ), 'iiif' );
+	my $IIIF_BASE_PATH = $plugin->param( 'iiif_base_path' ) || $eprint->value( 'dir' );
+	my $IIIF_PATH_FIELD = $plugin->param( 'iiif_path_field' );
 
 	my $data = {
 		'@context' => 'http://iiif.io/api/presentation/2/context.json',
 		'@id' => $id,
 		'@type' => 'sc:Manifest',
-		label => $dataobj->value( 'title' ),
+		label => $eprint->value( 'title' ),
 		sequences => [
 			{ 
 				'@id' => sprintf( '%s/sequence/0', $id ),
@@ -48,17 +49,41 @@ print STDERR "output dataobj\n";
 		metadata => [
 			{
 				label => 'Title',
-				value => $dataobj->value( 'title' ),
+				value => $eprint->value( 'title' ),
 			}
 		],
 	};
 
+	my @docs;
+	if( $eprint->value( 'type' ) eq 'collection' )
+	{
+		# gather parts
+		my $parts = $repo->dataset( 'eprint' )->search(
+			{
+				filters => [
+					{ metadata_fields => [qw( relation_type )], 'http://purl.org/dc/terms/isPart', 'EX' },
+					{ metadata_fields => [qw( relation_uri )], $eprint->internal_uri, 'EX' },
+				],
+				satisfy_all => 1,
+				custom_order => 'placement',
+			}
+		);
+		$parts->map( $repo, sub {
+			my( undef, undef, $eprint, $docs ) = @_;
+
+			push @$docs, $eprint->get_all_documents;
+		}, \@docs );
+	}
+	else
+	{
+		@docs = $eprint->get_all_documents;
+	}
+
 	my @canvases;
-	my @docs = $dataobj->get_all_documents;
 	for( my $i = 0; $i < scalar @docs; $i++ )
 	{
 		my $doc = $docs[$i];
-		my $path = sprintf( '%s/%s', $IMAGE_SERVER_BASE, $doc->exists_and_set( 'relative_path' ) ? $doc->value( 'relative_path' ) : $doc->value( 'main' ) );
+		my $path = sprintf( '%s/%s', $IIIF_BASE_PATH, $doc->exists_and_set( $IIIF_PATH_FIELD ) ? $doc->value( $IIIF_PATH_FIELD ) : $doc->value( 'main' ) );
 		my $canvas = sprintf( '%s/canvas/%s/%d', $id, $path, $i );
 
 		push @canvases, {
@@ -74,7 +99,7 @@ print STDERR "output dataobj\n";
 						'@type' => 'dcTypes =>Image',
 						service => {
 							'@context' => 'http://iiif.io/api/image/2/context.json',
-							'@id' => sprintf( '%s/%s', $IMAGE_SERVER_URL, $path ),
+							'@id' => sprintf( '%s/%s', $IIIF_SERVER_URL, $path ),
 							profile => 'http://iiif.io/api/image/2/level1.json'
 						}
 					},
